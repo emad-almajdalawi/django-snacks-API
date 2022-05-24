256c19f35ea6e538f5b2e0c48123dbb4f9c8a2f9
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-25 00:32:34 +0300

    fix: add ForeinKey

--- my_snacks_api/api/serializers.py
+++ my_snacks_api/api/serializers.py
@@ -4,5 +4,5 @@ from my_snacks_api.models import Snack
 
 class SnackSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id','title', 'description', 'calories')
+        fields = ('id','title', 'description', 'calories', 'user')
         model = Snack
--- my_snacks_api/models.py
+++ my_snacks_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Snack(models.Model):
     title = models.CharField(max_length=255)
@@ -6,6 +7,7 @@ class Snack(models.Model):
     calories = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
